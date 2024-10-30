bb9921a366c51553561840d1f735258d49ffc5b1
Lucasb0202 <lucab1602@gmail.com>
2024-10-30 15:56:57 +0200

    max width changes globals.css

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -409,7 +409,7 @@ export default function Home() {
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
               transition={{ type: "spring", stiffness: 400, damping: 10 }}
-              >
+            >
               <a 
                 href="https://www.instagram.com/bsc_za/" 
                 // className="z-10"
